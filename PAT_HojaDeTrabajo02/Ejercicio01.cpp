495c941af7df14edafb96bd92815785dac10ae91
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-21 22:45:32 -0600

    Update Ejercicio01.cpp

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -2,5 +2,39 @@
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    return nullptr;
+    if (!head || k == 0) {
+        return head;
+    }
+
+    int length = 1;
+    Node<char>* current = head;
+
+    // Calcula la longitud de la lista
+    while (current->next) {
+        length++;
+        current = current->next;
+    }
+
+    k = k % length;  // Posición efectiva de rotación
+
+    if (k == 0) {
+        return head;  // No es necesario rotar
+    }
+
+    // Encuentra el nuevo nodo principal después de la rotación
+    Node<char>* new_head = head;
+    for (int i = 0; i < length - k - 1; i++) {
+        new_head = new_head->next;
+    }
+
+    // Actualiza los punteros para realizar la rotación
+    Node<char>* new_tail = new_head;
+    while (new_tail->next) {
+        new_tail = new_tail->next;
+    }
+    new_tail->next = head;
+    head = new_head->next;
+    new_head->next = nullptr;
+
+    return head;
 }
