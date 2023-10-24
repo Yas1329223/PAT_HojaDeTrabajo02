db6b4b297001c9d08b08550123ec2f544dfcb302
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-23 18:14:00 -0600

    Update Ejercicio02.cpp

--- PAT_HojaDeTrabajo02/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio02.cpp
@@ -2,5 +2,36 @@
 
 Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2)
 {
-    return nullptr;
+    Node<int>* dummyHead = new Node<int>(0);
+    Node<int>* current = dummyHead;
+    int carry = 0;
+
+    while (l1 || l2)
+        {
+        int sum = carry;
+
+        if (l1) 
+        {
+            sum += l1->data;
+            l1 = l1->next;
+        }
+
+        if (l2)
+        {
+            sum += l2->data;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+        current->next = new Node<int>(sum % 10);
+        current = current->next;
+    }
+
+    if (carry > 0) {
+        current->next = new Node<int>(carry);
+    }
+
+    Node<int>* result = dummyHead->next;
+    delete dummyHead;
+    return result
 }
