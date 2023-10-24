a24e100641b85a3ba587e72b73a9eb3851b1f555
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-23 21:23:53 -0600

    Update Ejercicio03.cpp

--- PAT_HojaDeTrabajo02/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio03.cpp
@@ -1,46 +1,63 @@
 #include "Ejercicio03.h"
+#include <stack>
+#include <cctype>
+using namespace std;
 
 int Ejercicio03::calculate(string s)
 {
-    stack<int> operands;
-    int num = 0;
-    char op = '+';
-
-    for (char c : s)
+    if (s.empty()) 
     {
-        if (isdigit(c)) {
-            num = num * 10 + (c - '0');
-        }
-        else if (c != ' ')
-        {
-            if (op == '+') 
-            {
-                operands.push(num);
-            } else if (op == '-') 
-            {
-                operands.push(-num);
-            } else if (op == '*')
-            {
-                int prev = operands.top();
-                operands.pop();
-                operands.push(prev * num);
-            } else if (op == '/') 
-            {
-                int prev = operands.top();
-                operands.pop();
-                operands.push(prev / num);
-            }
+        return 0;
+    }
 
-            op = c;
-            num = 0;
+    stack<int> operandStack;
+    char currentOperator = '+';
+
+    for (size_t i = 0; i < s.length(); i++) 
+    {
+        if (s[i] == ' ') 
+        {
+            continue;
+        } else if (isdigit(s[i]))
+        {
+            string numberStr = "";
+            while (isdigit(s[i])) 
+            {
+                numberStr += s[i];
+                i++;
+            }
+            i--;
+
+            int number = stoi(numberStr);
+
+            if (currentOperator == '+') 
+            {
+                operandStack.push(number);
+            } else if (currentOperator == '-')
+            {
+                operandStack.push(-number);
+            } else if (currentOperator == '*')
+            {
+                int previousOperand = operandStack.top();
+                operandStack.pop();
+                operandStack.push(previousOperand * number);
+            } else if (currentOperator == '/')
+            {
+                int previousOperand = operandStack.top();
+                operandStack.pop();
+                operandStack.push(previousOperand / number);
+            }
+        } else 
+        {
+            currentOperator = s[i];
         }
     }
 
     int result = 0;
-    while (!operands.empty())
+    while (!operandStack.empty()) 
     {
-        result += operands.top();
-        operands.pop();
+        result += operandStack.top();
+        operandStack.pop();
     }
 
     return result;
