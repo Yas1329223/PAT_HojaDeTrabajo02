d54a775f4ea880d502fa78439593fe49b361b5ab
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-23 21:14:22 -0600

    Update Ejercicio02.cpp

--- PAT_HojaDeTrabajo02/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio02.cpp
@@ -2,39 +2,65 @@
 #include <string>
 using namespace std;
 
-Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2) 
+Node<int>* Ejercicio02::addTwoNumbers(Node<int>* list1, Node<int>* list2)
 {
-    Node<int>* dummyHead = new Node<int>(0);
-    Node<int>* current = dummyHead;
-    int carry = 0;
-
-    while (l1 || l2)
+    if (list1 == nullptr) 
     {
-        int sum = carry;
-
-        if (l1) 
-        {
-            sum += l1->data;
-            l1 = l1->next;
-        }
-
-        if (l2) 
-        {
-            sum += l2->data;
-            l2 = l2->next;
-        }
-
-        carry = sum / 10;
-        current->next = new Node<int>(sum % 10);
-        current = current->next;
+        return list2;
+    } else if (list2 == nullptr) 
+    {
+        return list1;
     }
 
-    if (carry > 0)
+    int count1 = 1;
+    int count2 = 1;
+    Node<int>* current1 = list1;
+    Node<int>* current2 = list2;
+
+    while (current1->next != nullptr) 
     {
-        current->next = new Node<int>(carry);
+        count1++;
+        current1 = current1->next;
     }
 
-    Node<int>* result = dummyHead->next;
-    delete dummyHead;
-    return result;
+    while (current2->next != nullptr) 
+    {
+        count2++;
+        current2 = current2->next;
+    }
+
+    if ((count1 >= 1 && count1 <= 360) && (count2 >= 1 && count2 <= 360))
+    {
+        Node<int>* dummy = new Node<int>{ 0 };
+        Node<int>* temp = dummy;
+        int carry = 0;
+
+        while (list1 != nullptr || list2 != nullptr || carry != 0) 
+        {
+            int digit1 = 0;
+            int digit2 = 0;
+
+            if (list1) 
+            {
+                digit1 = list1->value;
+                list1 = list1->next;
+            }
+            
+            if (list2) 
+            {
+                digit2 = list2->value;
+                list2 = list2->next;
+            }
+
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+            Node<int>* newNode = new Node<int>{ sum % 10 };
+            temp->next = newNode;
+            temp = temp->next;
+        }
+        
+        return dummy->next;
+    }
+    
+    return nullptr;
 }
