92b66bbab9faf999f6abf7505e67c688106bdca4
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-23 21:41:46 -0600

    Update Ejercicio04.cpp

--- PAT_HojaDeTrabajo02/Ejercicio04.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio04.cpp
@@ -1,60 +1,197 @@
 #include "Ejercicio04.h"
-#include <deque>
 
 FrontMiddleBackQueue::FrontMiddleBackQueue()
 {
+	front = nullptr;
+	middle = nullptr;
+	back = nullptr;
 }
 
 void FrontMiddleBackQueue::pushFront(int value)
 {
-    queue.push_front(value);
+	Nodo* newNode = new Nodo{ value };
+	if (front == nullptr)
+    {
+		front = newNode;
+		middle = newNode;
+		back = newNode;
+	}
+	else {
+		newNode->next = front;
+		front = newNode;
+		if (tamanio % 2 == 0) {
+			middle = middle->next;
+		}
+	}
+	tamanio++;
 }
 
 void FrontMiddleBackQueue::pushMiddle(int value)
 {
-    int middle = queue.size() / 2;
-    queue.insert(queue.begin() + middle, value);
+	Nodo* newNode = new Nodo{ value };
+
+	if (front == nullptr)
+    {
+		front = newNode;
+		middle = newNode;
+		back = newNode;
+	}
+	else if (tamanio == 1)
+    {
+		newNode->next = front;
+		back = front;
+		front = newNode;
+		middle = front;
+	}
+	else {
+		Nodo* fast = front;
+		Nodo* slow = front;
+		Nodo* prev = nullptr;
+		while (fast != nullptr && fast->next != nullptr) {
+			fast = fast->next->next;
+			prev = slow;
+			slow = slow->next;
+		}
+		if (prev != nullptr)
+        {
+			prev->next = newNode;
+		}
+		else 
+        {
+			front = newNode;
+		}
+		newNode->next = slow;
+		if (slow == nullptr)
+        {
+			back = newNode;
+		}
+		else if (tamanio % 2 == 0)
+        {
+			middle = middle->next;
+		}
+	}
+	tamanio++;
 }
 
-void FrontMiddleBackQueue::pushBack(int value) 
+void FrontMiddleBackQueue::pushBack(int value)
 {
-    queue.push_back(value);
+	Nodo* newNode = new Nodo{ value };
+	if (back == nullptr) {
+		front = newNode;
+		middle = newNode;
+		back = newNode;
+	}
+	else
+    {
+		back->next = newNode;
+		back = newNode;
+		if (tamanio % 2 == 1) 
+        {
+			middle = middle->next;
+		}
+	}
+	tamanio++;
 }
 
 int FrontMiddleBackQueue::popFront()
 {
-    if (queue.empty()) 
+	if (front == nullptr)
     {
-        return -1;
-    }
-
-    int front = queue.front();
-    queue.pop_front();
-    return front;
+		return -1;
+	}
+	int value = front->value;
+	if (front == back) 
+    {
+		delete front;
+		front = nullptr;
+		middle = nullptr;
+		back = nullptr;
+	}
+	else {
+		Nodo* temp = front;
+		front = front->next;
+		delete temp;
+		if (tamanio % 2 == 1) 
+        {
+			middle = middle->next;
+		}
+	}
+	tamanio--;
+	return value;
 }
 
-int FrontMiddleBackQueue::popMiddle() 
+int FrontMiddleBackQueue::popMiddle()
 {
-    if (queue.empty())
+	if (front == nullptr) 
     {
-        return -1;
-    }
+		return -1;
+	}
 
-    int middle = queue.size() / 2;
-    int value = queue[middle];
-    queue.erase(queue.begin() + middle);
-    return value;
+	if (front == back)
+    {
+		int value = front->value;
+		delete front;
+		front = nullptr;
+		middle = nullptr;
+		back = nullptr;
+		tamanio--;
+		return value;
+	}
+	else {
+		Nodo* fast = front;
+		Nodo* slow = front;
+		Nodo* prev = nullptr;
+		while (fast != nullptr && fast->next != nullptr && fast->next->next != nullptr) {
+			fast = fast->next->next;
+			prev = slow;
+			slow = slow->next;
+		}
+		int value = slow->value;
+		if (prev != nullptr)
+        {
+			prev->next = slow->next;
+		}
+		else {
+			front = slow->next;
+		}
+		delete slow;
+		tamanio--;
+		if (tamanio % 2 == 0)
+        {
+			middle = middle->next;
+		}
+		return value;
+	}
 }
 
 int FrontMiddleBackQueue::popBack()
 {
-    if (queue.empty()) 
+	if (front == nullptr)
     {
-        return -1;
-    }
-
-    int back = queue.back();
-    queue.pop_back();
-    return back;
+		return -1;
+	}
+	int value = back->value;
+	if (front == back)
+    {
+		delete front;
+		front = nullptr;
+		middle = nullptr;
+		back = nullptr;
+	}
+	else {
+		Nodo* temp = front;
+		while (temp->next != back) 
+        {
+			temp = temp->next;
+		}
+		delete back;
+		back = temp;
+		back->next = nullptr;
+		if (tamanio % 2 == 0)
+        {
+			middle = middle->next;
+		}
+	}
+	tamanio--;
+	return value;
 }
-
