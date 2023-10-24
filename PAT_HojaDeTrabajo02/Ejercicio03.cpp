aa5c8715f64714d9a4bbdf69e71e77c846993f0e
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-23 18:19:19 -0600

    Update Ejercicio03.cpp

--- PAT_HojaDeTrabajo02/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio03.cpp
@@ -2,5 +2,46 @@
 
 int Ejercicio03::calculate(string s)
 {
-    return 0;
+    stack<int> operands;
+    int num = 0;
+    char op = '+';
+
+    for (char c : s)
+    {
+        if (isdigit(c)) {
+            num = num * 10 + (c - '0');
+        }
+        else if (c != ' ')
+        {
+            if (op == '+') 
+            {
+                operands.push(num);
+            } else if (op == '-') 
+            {
+                operands.push(-num);
+            } else if (op == '*')
+            {
+                int prev = operands.top();
+                operands.pop();
+                operands.push(prev * num);
+            } else if (op == '/') 
+            {
+                int prev = operands.top();
+                operands.pop();
+                operands.push(prev / num);
+            }
+
+            op = c;
+            num = 0;
+        }
+    }
+
+    int result = 0;
+    while (!operands.empty())
+    {
+        result += operands.top();
+        operands.pop();
+    }
+
+    return result;
 }
