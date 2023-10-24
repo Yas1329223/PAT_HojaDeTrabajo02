61f3c8e8011e1612cfe5e2b787d1a5c8bd0e98b9
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-23 18:23:17 -0600

    Update Ejercicio02.cpp

--- PAT_HojaDeTrabajo02/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio02.cpp
@@ -1,13 +1,13 @@
 #include "Ejercicio02.h"
 
-Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2)
+Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2) 
 {
     Node<int>* dummyHead = new Node<int>(0);
     Node<int>* current = dummyHead;
     int carry = 0;
 
     while (l1 || l2)
-        {
+    {
         int sum = carry;
 
         if (l1) 
@@ -16,7 +16,7 @@ Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2)
             l1 = l1->next;
         }
 
-        if (l2)
+        if (l2) 
         {
             sum += l2->data;
             l2 = l2->next;
@@ -27,11 +27,12 @@ Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2)
         current = current->next;
     }
 
-    if (carry > 0) {
+    if (carry > 0)
+    {
         current->next = new Node<int>(carry);
     }
 
     Node<int>* result = dummyHead->next;
     delete dummyHead;
-    return result
+    return result;
 }
