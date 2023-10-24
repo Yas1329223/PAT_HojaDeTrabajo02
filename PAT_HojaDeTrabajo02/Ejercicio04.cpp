056babe7b2f189a7b7c177a628ffc2d2852b8145
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-23 18:29:27 -0600

    Update Ejercicio04.cpp

--- PAT_HojaDeTrabajo02/Ejercicio04.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio04.cpp
@@ -3,30 +3,55 @@
 FrontMiddleBackQueue::FrontMiddleBackQueue()
 {
 }
-
 void FrontMiddleBackQueue::pushFront(int value)
 {
+    queue.push_front(value);
 }
 
 void FrontMiddleBackQueue::pushMiddle(int value)
 {
+    int middle = queue.size() / 2;
+    queue.insert(queue.begin() + middle, value);
 }
 
-void FrontMiddleBackQueue::pushBack(int value)
+void FrontMiddleBackQueue::pushBack(int value) 
 {
+    queue.push_back(value);
 }
 
 int FrontMiddleBackQueue::popFront()
 {
-	return 0;
+    if (queue.empty()) 
+    {
+        return -1;
+    }
+
+    int front = queue.front();
+    queue.pop_front();
+    return front;
 }
 
-int FrontMiddleBackQueue::popMiddle()
+int FrontMiddleBackQueue::popMiddle() 
 {
-	return 0;
+    if (queue.empty())
+    {
+        return -1;
+    }
+
+    int middle = queue.size() / 2;
+    int value = queue[middle];
+    queue.erase(queue.begin() + middle);
+    return value;
 }
 
 int FrontMiddleBackQueue::popBack()
 {
-	return 0;
+    if (queue.empty()) 
+    {
+        return -1;
+    }
+
+    int back = queue.back();
+    queue.pop_back();
+    return back;
 }
